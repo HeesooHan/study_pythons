f26525dade0d85ef805dbf222db5daf868ef05df
HeesooHan <hhs950824@gmail.com>
2023-08-01 18:22:31 +0900

    2023-08-01

---
+++ datatype_list.py
@@ -0,0 +1,17 @@
+# slicing
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+type(thislist)
+# <class 'list'>
+len(thislist)
+# 7
+thislist[1:3]
+# ['banana', 'cherry']
+thislist[:-1]
+# ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon']
+
+# change value in list
+thislist[1] = 'watermelon'
+thislist[1:3] = ['cherry', 'watermelon']
+thislist.sort()
+thislist.sort(reverse=True)
+pass
--- datatype_strings.py
+++ datatype_strings.py
@@ -1,10 +1,37 @@
-str = "Yojulab !"
-len(str)
+stringing = "Yojulab !"
+len(stringing)
 pass
 
-# len(str)
+len(stringing)
 # 9
-# "ju" in str
+
+# condition
+"ju" in stringing
 # True
-# "Not" not in str
+"Not" in stringing
+# False
+"Not" not in stringing
 # True
+
+# https://www.w3schools.com/python/python_stringingings_slicing.asp
+# slicing
+stringing[3]
+# 'u'
+stringing[3:6]
+# 'ula'
+stringing[3:]
+# 'ulab !'
+stringing[:-2]
+# 'Yojulab'
+pass
+
+# format
+age = 36
+txt = "My name is John, I am " + str(age)
+print(txt)
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {0} pieces of item {1} for {2} dollars. I have just {2}."
+pass
